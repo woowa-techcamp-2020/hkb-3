0b5ec26d4d6be6e7e2d887c36793a643e5aef0cb
myungwoo-Y <auddn6676@naver.com>
2020-07-28 20:54:51 +0900

    [추가][3] add swagger #3 swagger 가 연동되게 추가했습니다! 또 ts 가 build 되는것을 고려해서 .gitignore 도 추가했어요!

--- server/src/app.ts
+++ server/src/app.ts
@@ -3,6 +3,8 @@ import path from 'path';
 
 import cookieParser from 'cookie-parser';
 import logger from 'morgan';
+import swaggerUi from 'swagger-ui-express';
+import swaggerJSDoc from 'swagger-jsdoc';
 import indexRouter from './routes/index';
 import usersRouter from './routes/users';
 
@@ -16,5 +18,33 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
+
+/** ************************************
+ *               Set Swagger
+ *************************************** */
+const swaggerDefinition = {
+  info: { // API informations (required)
+    title: 'HKB APIS', // Title (required)
+    version: '1.0.0', // Version (required)
+    description: '', // Description (optional)
+  },
+  host: 'localhost:3000', // Host (optional)
+  basePath: '/', // Base path (optional)
+};
+  
+// Options for the swagger docs
+const options = {
+  // Import swaggerDefinitions
+  swaggerDefinition,
+  // Path to the API docs
+  apis: ['./src/routes/users.*'],
+};
+
+const swaggerSpec = swaggerJSDoc(options);
+app.use('/api-docs', swaggerUi.serve);
+app.get('/api-docs', swaggerUi.setup(swaggerSpec));
+
+
 
 export default app;
