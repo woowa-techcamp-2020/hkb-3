6e476fbef64705a27176bd149faf839de5e58a3e
myungwoo-Y <auddn6676@naver.com>
2020-07-31 03:26:09 +0900

    [추가][#29] 달력 내용 표시 현재 달에 대한 달력을 표시하도록 했습니다!

--- client/src/views/Calendar/tableView.js
+++ client/src/views/Calendar/tableView.js
@@ -3,14 +3,73 @@ class TableView {
     this.wrap = document.querySelector('.content-wrap');
     this.rowCounts = 5;
     this.colCounts = 7;
+    this.setDate();
+  }
+
+
+  setDate() {
+    const todayDate = new Date();
+
+    // 요일
+    this.day = todayDate.getDay();
+    this.month = todayDate.getMonth();
+    // 특정 날짜
+    this.date = todayDate.getDate();
+    this.year = todayDate.getFullYear();
+
+    // 날짜
+    const startDate = new Date(this.year, this.month);
+    this.startDay = startDate.getDay();
+    // 마지막 날짜
+    this.lastDate = new Date(this.year, this.month + 1, 0).getDate();
+    this.previousLastDate = new Date(this.year, this.month, 0).getDate();
+  }
+
+  setRowCount() {
+    const totalDate = this.startDay + this.lastDate;
+    const maxCellCounts = 7 * 5;
+    if(totalDate > maxCellCounts) {
+      this.rowCounts = 6;
+    }else{
+      this.rowCounts = 5;
+    }
+  }
+
+  getClass = (idx, accessDateFlag) => {
+    let classes = 'header';
+    classes += !idx ? ' sunday' : '';
+    classes += !accessDateFlag ? ' other-date' : '';
+    return classes; 
   }
 
   buildContents = () => {
     let tableContents = '';
+    let dateCount = -this.startDay;
+    this.setRowCount();
+    
     for(let i = 0; i < this.rowCounts; i++) {
       tableContents += '<div class="date-grid">';
       for(let j = 0; j < this.colCounts; j++) {
-        tableContents += '<div>1</div>';
+        let date = 0;
+        let accessDateFlag = false;
+        if(dateCount < 0) {
+          date = this.previousLastDate + dateCount + 1;
+        }else if(dateCount / this.lastDate < 1) {
+          date = dateCount + 1;
+          accessDateFlag = true;
+        }else{
+          date = (dateCount % this.lastDate) + 1;
+        }
+        tableContents += `
+          <div class="day-wrap">
+            <div class="${this.getClass(j, accessDateFlag)}">
+              ${date}
+            </div>
+            <div>
+            </div>
+          </div>
+        `;
+        dateCount++;
       }
       tableContents += '</div>';
     }
@@ -28,7 +87,7 @@ class TableView {
             <div>수</div>
             <div>목</div>
             <div>금</div>
-            <div class="saturday">토</div>
+            <div>토</div>
         </div>
         ${this.buildContents()}
       </div>
