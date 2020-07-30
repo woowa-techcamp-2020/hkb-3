ba3b436a2014f6516bddafcce38af4aefefe0df5
myungwoo-Y <auddn6676@naver.com>
2020-07-30 17:18:36 +0900

    [추가][#29] 달력 기본 구조 그리드를 이용하여 달력 기본 구조를 추가했습니다

---
+++ client/src/views/Calendar/tableView.js
@@ -0,0 +1,40 @@
+class TableView {
+  constructor() {
+    this.wrap = document.querySelector('.content-wrap');
+    this.rowCounts = 5;
+    this.colCounts = 7;
+  }
+
+  buildContents = () => {
+    let tableContents = '';
+    for(let i = 0; i < this.rowCounts; i++) {
+      tableContents += '<div class="date-grid">';
+      for(let j = 0; j < this.colCounts; j++) {
+        tableContents += '<div>1</div>';
+      }
+      tableContents += '</div>';
+    }
+    return tableContents;
+  }
+
+  render() {
+    // eslint-disable-next-line no-undef
+    const tableHtml = `
+      <div class="calendar">
+        <div class="day-of-week">
+            <div class="sunday">일</div>
+            <div>월</div>
+            <div>화</div>
+            <div>수</div>
+            <div>목</div>
+            <div>금</div>
+            <div class="saturday">토</div>
+        </div>
+        ${this.buildContents()}
+      </div>
+    `;
+    this.wrap.innerHTML = tableHtml;
+  }
+}
+
+export default TableView;
