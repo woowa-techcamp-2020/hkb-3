3c0d8c1592408d0dbafda9e2056bd1b7edd9fb6b
myungwoo-Y <auddn6676@naver.com>
2020-08-03 14:33:44 +0900

    [수정][#7] dependency cycle 제거 기존 common.js에서 view를 import 하여 cycle이 발생하던 문제를 문제 소스들을 모두 router에 집어넣어 해결했습니다. 또 router를 model -> contoller로 변견했습니다.

--- client/src/common.js
+++ client/src/common.js
@@ -1,7 +1,3 @@
-import HomeView from './views/Home/homeView';
-import CalendarView from './views/Calendar';
-import StatisticsView from './views/Statistics';
-import RouterModel from './models/routerModel';
 import InitModel from './models/initModel';
 import CalendarModel from './models/calendarModel';
 import HomeModel from './models/homeModel';
@@ -22,15 +18,10 @@ export const $ = (str) => {
 };
 
 export const elements = {
-  routerModel: new RouterModel(),
   initModel: initModeltemp,
   calendarModel: new CalendarModel(),
   homeModel: new HomeModel(),
-  stastisticsModel: new StatisticsModel(),
-  calendarView: new CalendarView(),
-  homeView: new HomeView(initModeltemp),
-  statisticsView: new StatisticsView(),
-  
+  stastisticsModel: new StatisticsModel(),  
   contentWrap: document.querySelector('.content-wrap'),
 };
 
@@ -41,11 +32,6 @@ export const paths = {
   statistics: '/statistics',
 };
 
-export function getCurrentPath(e, listNode) {
-  if(e.target.nodeName === 'A') e.preventDefault(); 
-  const path = listNode.querySelector('a').getAttribute('href');
-  return path;
-}
 
 export function getState(path) {
   switch(path) {
@@ -56,38 +42,8 @@ export function getState(path) {
   case paths.statistics:
     return elements.stastisticsModel;
   default:
-    
-    break;
+    return null;
   }
-}
-
-const viewMap = {
-  [paths.home](state) {
-    elements.homeView.render(state);
-  },
-  [paths.calendar](state) {
-    elements.calendarView.render(state); 
-  },
-  [paths.statistics](state) {
-    elements.statisticsView.render(state);
-  },
-};
-
-export function renderByUrl({ state }) {
-  const targetView = getPath();
-  viewMap[targetView](state);
-}
-
-export function getPath() {
-  return location.pathname;
-}
-
-/**
- * @description 모델에 해당하는 url 이면 render 합니다.
- * @param {class} model 
- */
-export function renderByModel(model) {
-  if(getPath() === paths[model.name]) { renderByUrl(model); }
 }
 
 export const isPayment = (obj) => obj.state === '지출';
