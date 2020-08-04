50f4464559fe4c23a6a54055f7a013342bbcc063
이찬호 <0407chan@naver.com>
2020-08-04 22:46:45 +0900

    [추가][#28] 거래내역 api 추가
    - 거래내역 수정 추가
    - 거래내역 삭제 추가
    - 거래내역 조회 추가
    - 각각 api통신부분 추가

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -2,11 +2,13 @@ import express from 'express';
 import userRouter from './user';
 import loginRouter from './login';
 import transactionRouter from './transaction';
+import categoryRouter from './category';
 
 const router = express.Router();
 
 router.use('/api', userRouter);
 router.use('/api', transactionRouter);
+router.use('/api', categoryRouter);
 
 router.use('/auth', loginRouter);
 
