bb7c95793eac2a6e755bd3571f880261d4141fdc
myungwoo-Y <auddn6676@naver.com>
2020-08-06 15:21:10 +0900

    [추가][#37] 날에 따른 day graph 표시 먼저 polyline 과 circle 을 이용해서 그래프를 표시했습니다 그리도 keyframse 을 이용해서 그래프가 그려진 후 각 지점을 표시하도록 했습니다

--- client/src/views/Statistics/DayView/index.js
+++ client/src/views/Statistics/DayView/index.js
@@ -15,17 +15,18 @@ class DayWarp {
 
   buildBasicLine = () => {
     const lineNumber = 11;
-    const heightPadding = 50;
-    const defaultHeight = this.height - heightPadding;
-    const heightDiffer = 50;
-    const digit = 10;
+    this.heightPadding = 50;
+    const defaultHeight = this.height - this.heightPadding;
+    this.heightDiffer = 50;
     const xPadding = 20;
-    const maxSpend = this.getMaxSpend();
+    const amountDiffer = this.maxSpend / (lineNumber - 1);
     let content = `<line x1="${this.defaultX}" y1="${defaultHeight}" x2="100%" y2="${defaultHeight}" />`;
     new Array(lineNumber).fill().reduce((prev, cur, i) => {
-      const newHeight = prev - heightDiffer;
+      const newHeight = prev - this.heightDiffer;
       let amount = 0;
-      if(i !== 0) { amount = Math.round((maxSpend / (10000 * (lineNumber - i))) / digit) * digit; }
+      if(i !== 0) { 
+        amount = Math.round(((this.maxSpend - (lineNumber - i - 1) * amountDiffer) / 10000)); 
+      }
       content += `
         <line x1="${this.defaultX}" y1="${newHeight}" x2="100%" y2="${newHeight}" />
         <text x="${this.defaultX - xPadding}" y="${newHeight}" text-anchor="end">
@@ -41,15 +42,60 @@ class DayWarp {
     const lastDate = this.getLastDate();
     const dateNumber = 6;
     const differDate = 5;
-    const differX = ((this.width - this.defaultX) / parseInt(lastDate)) * differDate;
+    this.differX = ((this.width - this.defaultX) / parseInt(lastDate));
     const heightPadding = 20;
     let content = '';
     new Array(dateNumber).fill().reduce((prev, cur, i) => {
       content += `
-        <text x=${prev}, y=${this.height - heightPadding}>${this.month}.${this.date + differDate * i}</text>
+        <text x=${prev}, y=${this.height - heightPadding}>${this.month}.${1 + differDate * i}</text>
       `;
-      return prev + differX;
+      return prev + (this.differX * differDate);
     }, this.defaultX);
+    return content;
+  }
+
+  getListRemovedDateDuplicate() {
+    const obj = {};
+    this.state.data.forEach((payment) => {
+      const paymentDate = new Date(payment.date);
+      const key = paymentDate.getDate();
+      if(obj[key]) {
+        obj[key].amount += payment.amount;
+      }else{
+        obj[key] = { date: key, amount: payment.amount };
+      }
+    });
+    const list = Object.values(obj);
+    return list.sort((a, b) => a.date - b.date);
+  }
+
+  buildPointsLine() {
+    const numberOfPadding = 3;
+    const totalHeight = this.height - this.heightDiffer * numberOfPadding;
+    let content = '';
+    let points = '';
+    const sortByDateList = this.getListRemovedDateDuplicate();
+
+
+    sortByDateList.forEach((payment, i) => {
+      const paymentDate = payment.date;
+      const amountPercent = payment.amount / this.maxSpend;
+      // eslint-disable-next-line max-len
+      const heightAboutAmount = totalHeight - (totalHeight * amountPercent) + this.heightPadding;
+      const widthAboutAmount = this.defaultX + this.differX * paymentDate;
+      content += `
+        <circle cx="${widthAboutAmount}" cy="${heightAboutAmount}" r="5"/>
+      `;
+      if(i === sortByDateList.length - 1) {
+        points += `${widthAboutAmount} ${heightAboutAmount}`;
+      }else{
+        points += `${widthAboutAmount} ${heightAboutAmount}, `;
+      }
+    }, this);
+    content = `
+      <polyline points="${points}"/> 
+      ${content}
+    `;
     return content;
   }
 
@@ -59,19 +105,32 @@ class DayWarp {
       <svg width="${this.width}px" height="${this.height}px">
         ${this.buildBasicLine()}
         ${this.buildDates()}
+        ${this.buildPointsLine()}
       </svg>
     `;
-    this.getMaxSpend();
     return content;
   }
 
+  getMaxAmount() {
+    let maxAmount = 0;
+    console.log(this.getListRemovedDateDuplicate());
+    this.getListRemovedDateDuplicate().forEach((payment) => {
+      maxAmount = maxAmount > payment.amount ? maxAmount : payment.amount;
+    });
+    return maxAmount;
+  }
 
-  getMaxSpend() {
-    const strTotalSpend = this.state.totalSpend.toString();
+
+  setMaxSpend() {
+    const maxAmount = this.getMaxAmount();
+    if(maxAmount < 100) {
+      this.maxSpend = maxAmount;
+    }
+    const strMaxSpend = maxAmount.toString();
     const mid = 5;
-    let strNewTotalSpend = '';
-    let firstDigit = parseInt(strTotalSpend[0]);
-    let secondDigit = parseInt(strTotalSpend[1]);
+    let strNewMaxSpend = '';
+    let firstDigit = parseInt(strMaxSpend[0]);
+    let secondDigit = parseInt(strMaxSpend[1]);
 
     if(secondDigit >= mid) {
       secondDigit = 0;
@@ -79,18 +138,19 @@ class DayWarp {
     }else{
       secondDigit = mid;
     }
-    strNewTotalSpend = `${firstDigit}${secondDigit}${'0'.repeat(strTotalSpend.length - 2)}`;
+    strNewMaxSpend = `${firstDigit}${secondDigit}${'0'.repeat(strMaxSpend.length - 2)}`;
 
-    return parseInt(strNewTotalSpend);
+    this.maxSpend = parseInt(strNewMaxSpend);
   } 
 
 
 
   render = (state) => {
     this.state = state;
+    console.log(state);
+    this.setMaxSpend();
     this.state.wrap.innerHTML = `
         <div class="day-wrap">
-          <h1>Day Section</h1>
           ${this.buildLineGraph()}
         </div>
     `;
