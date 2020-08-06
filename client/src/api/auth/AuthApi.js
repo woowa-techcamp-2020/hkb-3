d899f9a7a0f01ff233bedd08ed4c453f16921b46
이찬호 <0407chan@naver.com>
2020-08-07 02:18:17 +0900

    [fix][#67] bug hunting
    작업 완료
    - 프론트에서 로그 아웃 api 생성

--- client/src/api/auth/AuthApi.js
+++ client/src/api/auth/AuthApi.js
@@ -10,6 +10,18 @@ async function getGitAuth() {
   return fetchResult;
 }
 
+async function logOut() {
+  const fetchResult = await fetch('/auth/logout', {
+    method: 'GET',
+    headers: { 'Content-Type': 'application/json' },
+  }).then(async (response) => {
+    const result = await response.json();
+    return result;
+  });
+  return fetchResult;
+}
+
 export default {
   getGitAuth,
+  logOut,
 };
