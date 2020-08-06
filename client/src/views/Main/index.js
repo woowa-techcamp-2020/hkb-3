4160de257bb64369f8beabc8fa7cc52eba117053
이찬호 <0407chan@naver.com>
2020-08-07 02:20:07 +0900

    [fix][#67] bug hunting
    작업 완료
    - 라우터 색 적용

--- client/src/views/Main/index.js
+++ client/src/views/Main/index.js
@@ -1,14 +1,21 @@
 import $ from '../../lib/miniJQuery';
 import { fillZeroToDate, elements } from '../../common';
 import View from '../view';
+import API from '../../api/index';
 
 const APP = '.app';
+
+const SELECTOR_HEADER_ROUTER_TRANSACTION_TEXT = '.header-router__transaction__text';
+const SELECTOR_HEADER_ROUTER_CALENDAR_TEXT = '.header-router__calendar__text';
+const SELECTOR_HEADER_ROUTER_STATISTICS_TEXT = '.header-router__statistics__text';
+
+const CLASS_CURRENT_ROUTER = 'js-current-router';
+
 
 class MainView extends View {
   constructor(data) {
     super(data);
     this.data = data;
-    this.wrap = $('.content-wrap').getNode();
     this.wrap = $(APP).getNode();
     this.state = null;
   }
@@ -29,21 +36,21 @@ class MainView extends View {
           </div>
           <nav class="header-router js-header-router">
             <div class="header-router__transaction">
-              <a class="header-router__transaction__text" href="/">Transaction History</a>
+              <a class="header-router__transaction__text js-header-router__transaction__text" href="/">Transaction History</a>
             </div>
             <div class="header-router__calendar">
-              <a class="header-router__calendar__text" href="/calendar">Calendar</a>
+              <a class="header-router__calendar__text js-header-router__calendar__text" href="/calendar">Calendar</a>
             </div>
             <div class="header-router__statistics">
-              <a class="header-router__statistics__text" href="/statistics">Statistics</a>
+              <a class="header-router__statistics__text js-header-router__statistics__text" href="/statistics">Statistics</a>
             </div>
           </nav>
           
           <div class="header-user-wrap">
-            <div calss="header-user">이찬호님</div>
+            <div calss="header-user">${elements.initModel.state.userInfo.name} 님</div>
           </div>
           <div class="header-btn-wrap">
-            <button class="header-btn">로그아웃</input>
+            <button class="header-btn js-header-logout-btn">로그아웃</input>
           </div>
         </div>
 
@@ -59,6 +66,29 @@ class MainView extends View {
 
     super.addHandler(() => $('.main-month-wrap').click((event) => this.dateChangeHandler(event)));
     return content;
+  }
+
+  addLogOutEvent = () => {
+    document.querySelector('.js-header-logout-btn').addEventListener('click', async () => {
+      window.location.href = '/auth/login';
+      const result = await API.Auth().logOut();
+    });
+  }
+
+  addRouterColorEvent = () => {
+    const transRouter = document.querySelector(SELECTOR_HEADER_ROUTER_TRANSACTION_TEXT);
+    const calendarRouter = document.querySelector(SELECTOR_HEADER_ROUTER_CALENDAR_TEXT);
+    const statisticsRouter = document.querySelector(SELECTOR_HEADER_ROUTER_STATISTICS_TEXT);
+
+    if(window.location.pathname === '/') {
+      transRouter.classList.add(CLASS_CURRENT_ROUTER);
+    }
+    if(window.location.pathname === '/calendar') {
+      calendarRouter.classList.add(CLASS_CURRENT_ROUTER);
+    }
+    if(window.location.pathname === '/statistics') {
+      statisticsRouter.classList.add(CLASS_CURRENT_ROUTER);
+    }
   }
 
   dateChangeHandler = (event) => {
@@ -80,6 +110,8 @@ class MainView extends View {
 
   render() {
     this.wrap.innerHTML = this.buildHeader();
+    this.addLogOutEvent();
+    this.addRouterColorEvent();
     super.notifyHandlers();
   }
 }
